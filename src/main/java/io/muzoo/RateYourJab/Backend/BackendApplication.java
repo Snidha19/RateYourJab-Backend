45c7d18bac08e0b119314c8e9bf24b52073a8cb4
IngJirayu <ingapivi@gmail.com>
2021-07-12 02:51:00 +0700

    added a custom user details service(Video2)

--- src/main/java/io/muzoo/RateYourJab/Backend/BackendApplication.java
+++ src/main/java/io/muzoo/RateYourJab/Backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package io.muzoo.RateYourJab.Backend.config;
+package io.muzoo.RateYourJab.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
