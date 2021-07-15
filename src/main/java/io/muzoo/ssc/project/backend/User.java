27d798cb77cd6a4767b06b8d78bfaa08871a33cd
IngJirayu <ingapivi@gmail.com>
2021-07-16 02:19:59 +0700

    database creating error fixed --finished video 4

--- src/main/java/io/muzoo/ssc/project/backend/User.java
+++ src/main/java/io/muzoo/ssc/project/backend/User.java
@@ -1,13 +1,11 @@
 package io.muzoo.ssc.project.backend;
 
 
+
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
 import javax.persistence.*;
-import java.time.LocalDate;
-
 
 @Entity
 @Getter
@@ -19,13 +17,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(unique = true)
     private String username;
 
-    private char gender;
-
-    private String fullname;
-
-    private LocalDate dateFormat;
+    private String password;
 
     private String role;
 }
