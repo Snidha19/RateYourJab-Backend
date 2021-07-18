b480caabed6593dcb7729bd304789fc7daa75320
Kriangsak1997 <kriangsak.thi@student.mahidol.edu>
2021-07-18 14:15:19 +0700

    user repo not yet working

---
+++ src/main/java/io/muzoo/ssc/RateYourJab/backend/User.java
@@ -0,0 +1,24 @@
+package io.muzoo.ssc.RateYourJab.backend;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name="tbl_user")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private long id;
+
+    private String username;
+    private String password;
+
+    private String role;
+
+
+}
