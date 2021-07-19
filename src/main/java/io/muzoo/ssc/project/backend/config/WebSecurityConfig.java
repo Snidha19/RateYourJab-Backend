8a8b90bab7dc48418cf63991dc9d0dacc3550d00
Kriangsak1997 <kriangsak.thi@student.mahidol.edu>
2021-07-19 13:28:15 +0700

    downloading postman

--- src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
+++ src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
@@ -4,13 +4,21 @@ import io.muzoo.ssc.project.backend.auth.OurUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -26,20 +34,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+		http.csrf().disable();
+		//permit root and api login, logout
+		http.authorizeRequests().antMatchers("/","/api/login","/api/logout").permitAll();
+		//permit all OPTION requests
+		http.authorizeRequests().antMatchers(HttpMethod.OPTIONS,"/**").permitAll();
+
+		//handle error output as JSON file for  unauth'ed access
+		http.exceptionHandling()
+				.authenticationEntryPoint(new JsonHttp403ForbiddenEntryPoint());
+
+		// set every other path to require authentication
+		http.authorizeRequests().antMatchers("/**").authenticated();
 	}
-
-
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
@@ -49,5 +56,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public UserDetailsService userDetailsService() {
 		return ourUserDetailsService;
+	}
+
+	class JsonHttp403ForbiddenEntryPoint implements AuthenticationEntryPoint{
+
+		@Override
+		public void commence(HttpServletRequest httpServletRequest,
+							 HttpServletResponse httpServletResponse,
+							 AuthenticationException e) throws IOException, ServletException {
+			//output JSON message
+			//for now just print the message out
+			httpServletResponse.getWriter().println("You are not allowed to access this page ;(");
+
+		}
 	}
 }
