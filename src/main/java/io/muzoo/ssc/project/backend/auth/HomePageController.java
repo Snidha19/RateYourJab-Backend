69823cc3859eb0dc51ab4d9a0bd3b3fbd3d34e52
Kriangsak1997 <kriangsak.thi@student.mahidol.edu>
2021-07-22 12:37:04 +0700

    trying out

--- src/main/java/io/muzoo/ssc/project/backend/auth/HomePageController.java
+++ src/main/java/io/muzoo/ssc/project/backend/auth/HomePageController.java
@@ -1,12 +1,32 @@
 package io.muzoo.ssc.project.backend.auth;
 
+import io.muzoo.ssc.project.backend.ReviewRepository;
+import io.muzoo.ssc.project.backend.model.Review;
+import io.muzoo.ssc.project.backend.whoami.ReviewDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 public class HomePageController {
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Autowired
+    ReviewDTO reviews;
     @GetMapping("/api/homepage")
-    //this will be directed only a user is logged in
+    //this will be directed only if a user is logged in
     //once the user is here, all the reviews will be rendered
-    public String home(){
-        return "If this message is shown, it means login is successful";
+    public ReviewDTO home(){
+        reviews.getReviews();
+        return reviews;
+    }
+
+
+
+    public ReviewDTO searchByTag(HttpServletRequest servletRequest){
+        reviews.getTaggedReviews();
+        return reviews;
     }
 }
